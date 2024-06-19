b1cff18548ed1cbad1cce063c69524ba839c98f2
Loes_s <103633598+LoesSsssSssssssssssssssssssss@users.noreply.github.com>
2024-06-19 12:24:34 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Modal from '../components/Modal';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
